bdec725d60840078c36228f37058c91577c303b3
gdh <gdhamateur@163.com>
2021-03-19 22:58:55 +0800

    结构调整

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import numpy as np
 from bert4keras.models import build_transformer_model
 from bert4keras.backend import keras, K
 from bert4keras.optimizers import Adam
@@ -43,12 +44,15 @@ def evaluate(data, model):
 class Evaluator(keras.callbacks.Callback):
     """评估与保存
     """
-    def __init__(self, model, data):
+    def __init__(self, model, data, fold_num):
         self.best_val_score = 0.
         self.data = data
         self.model = model
+        self.fold_num = fold_num
 
     def on_epoch_end(self, epoch, logs=None):
+        if epoch % 10 == 0:
+            self.model.save_weights('checkout/model-{}-{}.h5'.format(epoch, self.fold_num))
         val_score = evaluate(self.data, self.model)
         if val_score > self.best_val_score:
             self.best_val_score = val_score
@@ -57,3 +61,33 @@ class Evaluator(keras.callbacks.Callback):
             u'val_score: %.5f, best_val_score: %.5f\n' %
             (val_score, self.best_val_score)
         )
+
+
+class Warmup(keras.callbacks.Callback):
+    def __init__(self, lr_base=3e-5, lr_min=0.0, decay=0, warmup_epochs=2):
+        self.num_passed_batchs = 0
+        self.warmup_epochs = warmup_epochs
+        self.lr = lr_base
+        self.lr_min = lr_min
+        self.decay = decay
+        self.steps_per_epoch = 0
+
+    def on_batch_begin(self, batch, logs=None):
+        # params是模型自动传递给Callback的一些参数
+        if self.steps_per_epoch == 0:
+            # 防止跑验证集的时候呗更改了
+            if self.params['steps'] == None:
+                self.steps_per_epoch = np.ceil(1. * self.params['samples'] / self.params['batch_size'])
+            else:
+                self.steps_per_epoch = self.params['steps']
+        if self.num_passed_batchs < self.steps_per_epoch * self.warmup_epochs:
+            K.set_value(self.model.optimizer.lr,
+                        self.lr * (self.num_passed_batchs + 1) / self.steps_per_epoch / self.warmup_epochs)
+        else:
+            K.set_value(self.model.optimizer.lr,
+                        self.lr * ((1 - self.decay) ** (
+                                    self.num_passed_batchs - self.steps_per_epoch * self.warmup_epochs)))
+        self.num_passed_batchs += 1
+
+    def on_epoch_begin(self, epoch, logs=None):
+        print("learn-rate: {}".format(K.get_value(self.model.optimizer.lr)))
--- train.py
+++ train.py
@@ -3,19 +3,23 @@
 # 比赛链接：https://tianchi.aliyun.com/competition/entrance/531851
 
 import os
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import StratifiedKFold
+from bert4keras.backend import K
 from config import BaseConfig
 from util import load_data, get_tokens, get_keep_tokens, adversarial_training
-from model import get_model, Evaluator
+from model import get_model, Evaluator, Warmup
 from dataset import data_generator
+from random import seed
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+SEED = 2021
 # 加载数据集
 data = load_data(
     BaseConfig.train_path
 )
-train_data = [d for i, d in enumerate(data) if i % 10 != 0]
-valid_data = [d for i, d in enumerate(data) if i % 10 == 0]
 test_data = load_data(
     BaseConfig.test_path
 )
@@ -26,26 +30,40 @@ tokens = get_tokens(data+test_data)
 # BERT词频
 keep_tokens = get_keep_tokens()
 
-# 模拟未标注
-for d in valid_data + test_data:
-    train_data.append((d[0], d[1], -5))
-#
-# 加载预训练模型
-model = get_model(tokens, keep_tokens)
-#
-# 转换数据集
-train_generator = data_generator(train_data, BaseConfig.batch_size)
-valid_generator = data_generator(valid_data, BaseConfig.batch_size)
+# # 模拟未标注
+# for d in test_data:
+#     data.append((d[0], d[1], -5))
+labels = []
+for d in data:
+    labels.append(d[2])
 
 
 if __name__ == '__main__':
+    skf = StratifiedKFold(5, shuffle=True, random_state=2021)
+    for fold_id, (train_id, valid_id) in enumerate(skf.split(range(len(data)), labels)):
+        train_data = np.array(data)[train_id]
+        valid_data = np.array(data)[valid_id]
+        train_data = list(train_data)
+        valid_data = list(valid_data)
+        for d in valid_data + test_data:
+            train_data.append((d[0], d[1], -5))
+        train_generator = data_generator(train_data, BaseConfig.batch_size)
+        valid_generator = data_generator(valid_data, BaseConfig.batch_size)
+        K.clear_session()
+        seed(SEED + fold_id)
+        np.random.seed(SEED + fold_id)
+        tf.random.set_random_seed(SEED + fold_id)
+        # 加载预训练模型
+        model = get_model(tokens, keep_tokens)
+        adv_layer_names = ['Embedding-Token']
+        adversarial_training(model, adv_layer_names, 0.5)
+        evaluator = Evaluator(model, valid_generator, fold_id)
+        warmup = Warmup(decay=2e-5, warmup_epochs=5)
+        model.fit(
+            train_generator.forfit(),
+            steps_per_epoch=len(train_generator),
+            epochs=80,
+            callbacks=[evaluator, warmup]
+        )
+        del train_data, valid_data, model
 
-    evaluator = Evaluator(model, valid_generator)
-    adv_layer_names = ['Embedding-Token']
-    adversarial_training(model, adv_layer_names, 0.5)
-    model.fit(
-        train_generator.forfit(),
-        steps_per_epoch=len(train_generator),
-        epochs=100,
-        callbacks=[evaluator]
-    )
